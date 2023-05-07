accb4c13b87302201638005346192186644d247b
Vlad Epov <vladep622@gmail.com>
2023-05-07 16:20:27 +0300

    added book  index view

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,6 +1,7 @@
 const { useState } = React
 
 import { About } from "./views/about.jsx"
+import { BookIndex } from "./views/book-index.jsx"
 import { Home } from "./views/home.jsx"
 
 export function App() {
@@ -18,12 +19,14 @@ export function App() {
                 <h1>Miss Books App</h1>
                 <nav className="app-nav">
                     <a onClick={() => handlePageChange('home')} href="#">Home</a> |
+                    <a onClick={() => handlePageChange('book index')} href="#">Book Index</a> |
                     <a onClick={() => handlePageChange('about')} href="#">About</a>
                 </nav>
             </header>
             <main>
                 {page === 'home' && <Home />}
                 {page === 'about' && <About />}
+                {page === 'book index' && <BookIndex />}
             </main>
         </section>
     )
---
+++ views/book-index.jsx
@@ -0,0 +1,10 @@
+export function BookIndex() {
+
+    return (
+        <section className="book-list">
+            <h1>Hello from book Index</h1>
+        </section>
+    )
+
+
+}
