75852cd7df619e7c9f35f2f5ab415092dce54260
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-05-07 17:31:20 +0300

    added :

--- cmps/book-preview.jsx
+++ cmps/book-preview.jsx
@@ -3,7 +3,7 @@ export function BookPreview({ book }) {
         <article className="book-preview">
             <h2>Title: {book.title}</h2>
             <h4>Subtitle: {book.subtitle}</h4>
-            <h4>Description {book.description}</h4>
+            <h4>Description: {book.description}</h4>
         </article>
     )
 }
