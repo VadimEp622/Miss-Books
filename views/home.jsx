63e1296882ce75476cbe855a2d97f89f31342bd1
Vlad Epov <vladep622@gmail.com>
2023-05-07 15:25:01 +0300

    some css touch ups before starter ready

--- views/home.jsx
+++ views/home.jsx
@@ -1,8 +1,7 @@
 export function Home() {
     return (
         <section className="home">
-            <h1>Welcome To Books's R Us!</h1>
-            <img src="../assets/img/react.png" alt="" />
+            <h1>Welcome To Miss Books!</h1>
         </section>
     )
 }
