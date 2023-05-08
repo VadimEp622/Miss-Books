78472f42b1f5a8d8a7ee8f9bf73b9a66cb99302e
Vlad Epov <vladep622@gmail.com>
2023-05-08 21:55:57 +0300

    changed filter placeholders

--- cmps/book-filter.jsx
+++ cmps/book-filter.jsx
@@ -29,11 +29,11 @@ export function BookFilter({ filterBy, onSetFilter }) {
             <h2>Filter Our Books</h2>
 
             <form onSubmit={onSubmitFilter}>
-                <label htmlFor="tnamext">Name:</label>
-                <input value={name} onChange={handleChange} name="name" id="name" type="text" placeholder="By Vendor" />
+                <label htmlFor="name">Name:</label>
+                <input value={name} onChange={handleChange} name="name" id="name" type="text" placeholder="By Book Name" />
 
                 <label htmlFor="price">Price:</label>
-                <input value={price} onChange={handleChange} type="number" name="price" id="price" placeholder="By Min Speed" />
+                <input value={price} onChange={handleChange} type="number" name="price" id="price" placeholder="By Min Price" />
 
                 <button>Filter Books</button>
 
