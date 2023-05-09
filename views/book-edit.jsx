e0d8c60c0e12d293d87bfd747882db130b228095
Vlad Epov <vladep622@gmail.com>
2023-05-09 16:05:56 +0300

    cleaned up the useEffect in bookEdit jsx

--- views/book-edit.jsx
+++ views/book-edit.jsx
@@ -1,33 +1,29 @@
-const { useEffect, useState, useRef } = React
+const { useEffect, useState } = React
 const { useParams, useNavigate } = ReactRouterDOM
 
 import { bookService } from "../services/book.service.js"
 
 export function BookEdit() {
     const [bookToEdit, setBookToEdit] = useState(bookService.getEmptyBook())
+    const { bookId } = useParams()
     const navigate = useNavigate()
-    const params = useParams()
-    const didMountRef = useRef(false)
+
 
     useEffect(() => {
-        if (params.bookId) {
-            loadBook()
-        } else {
-            setBookToEdit(bookService.getEmptyBook())
-        }
-    }, [params.bookId])
+        if (bookId) loadBook()
+    }, [])
 
     useEffect(() => {
-        if (didMountRef.current) {
-            // console.log('bookToEdit', bookToEdit)
-        } else {
-            didMountRef.current = true
-        }
+        console.log('bookToEdit', bookToEdit)
     }, [bookToEdit])
 
+
+
+
+
     function loadBook() {
-        bookService.get(params.bookId)
-            .then(book => setBookToEdit(book))
+        bookService.get(bookId)
+            .then(setBookToEdit)
             .catch(err => {
                 console.log('Had issued in book edit:', err);
                 navigate('/book')
