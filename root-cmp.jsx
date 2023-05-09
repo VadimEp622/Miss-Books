d6147ff120bee5b81a5d25ca72d2f070d488d977
Vlad Epov <vladep622@gmail.com>
2023-05-09 16:33:56 +0300

    set errorMsgs to work, and added for some of the error catches, and some success thens

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -9,6 +9,8 @@ import { Home } from "./views/home.jsx"
 import { BookIndex } from "./views/book-index.jsx"
 import { BookEdit } from './views/book-edit.jsx'
 import { BookDetails } from "./views/book-details.jsx"
+import { UserMsg } from "./cmps/user-msg.jsx"
+
 
 export function App() {
 
@@ -35,6 +37,7 @@ export function App() {
                         <Route path="/book/edit" element={<BookEdit />} />
                     </Routes>
                 </main>
+                <UserMsg />
             </section>
         </Router>
     )
