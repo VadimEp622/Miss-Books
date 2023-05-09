c0e7a839d283c9157685dd3791cc392832a11e20
Vlad Epov <vladep622@gmail.com>
2023-05-09 16:49:22 +0300

    added functioning nested routes inside about view

---
+++ cmps/vision.jsx
@@ -0,0 +1,13 @@
+
+export function Vision() {
+
+    return (
+        <section className="vision">
+            <h2>Our vision is:</h2>
+            <ul>
+                <li>Sell lots of books</li>
+                <li>Make some money!!</li>
+            </ul>
+        </section>
+    )
+}
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -4,12 +4,14 @@ const Router = ReactRouterDOM.HashRouter
 const { Routes, Route } = ReactRouterDOM
 
 import { AppHeader } from "./cmps/app-header.jsx"
+import { UserMsg } from "./cmps/user-msg.jsx"
+import { Vision } from "./cmps/vision.jsx"
+import { Team } from "./cmps/team.jsx"
 import { About } from "./views/about.jsx"
 import { Home } from "./views/home.jsx"
 import { BookIndex } from "./views/book-index.jsx"
 import { BookEdit } from './views/book-edit.jsx'
 import { BookDetails } from "./views/book-details.jsx"
-import { UserMsg } from "./cmps/user-msg.jsx"
 
 
 export function App() {
@@ -30,7 +32,10 @@ export function App() {
                 <main>
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
+                        <Route path="/about" element={<About />} >
+                            <Route path="/about/vision" element={<Vision />} />
+                            <Route path="/about/team" element={<Team />} />
+                        </Route>
                         <Route path="/book/:bookId" element={<BookDetails />} />
                         <Route path="/book" element={<BookIndex />} />
                         <Route path="/book/edit/:bookId" element={<BookEdit />} />
@@ -39,6 +44,6 @@ export function App() {
                 </main>
                 <UserMsg />
             </section>
-        </Router>
+        </Router >
     )
 }
